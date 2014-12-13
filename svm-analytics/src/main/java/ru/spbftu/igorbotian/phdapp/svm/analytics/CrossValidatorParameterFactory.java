e2af6c9a3c5e54adeb8df2d1ff312756b971b2aa
Igor Botian <igor.botian@gmail.com>
2014-12-13 20:49:37 +0300

    Возможность задания значений параметра средства кросс-валидации

--- svm-analytics/src/main/java/ru/spbftu/igorbotian/phdapp/svm/analytics/CrossValidatorParameterFactory.java
+++ svm-analytics/src/main/java/ru/spbftu/igorbotian/phdapp/svm/analytics/CrossValidatorParameterFactory.java
@@ -6,30 +6,253 @@ package ru.spbftu.igorbotian.phdapp.svm.analytics;
 public interface CrossValidatorParameterFactory {
 
     /**
-     * Создание параметра, задающего количество генерируемых выборок
-     *
-     * @return параметр средства кросс-валидации
+     * Идентификатор параметра, задающего количество генерируемых выборок
      */
-    MutableCrossValidatorParameter<Integer> newSamplesToGenerateCountParameter();
+    public static final String SAMPLES_TO_GENERATE_COUNT_PARAM_ID = "samplesToGenerateCount";
 
     /**
-     * Создание параметра, задающего количество объектов в генерируемой выборке, т.е. её размер
-     *
-     * @return параметр средства кросс-валидации
+     * Значение по умолчанию параметра, задающего количество генерируемых выборок
      */
-    MutableCrossValidatorParameter<Integer> newSampleSizeParameter();
+    public static final int SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_VALUE = 100;
 
     /**
-     * Создание параметра, задающего соотношение количества объектов, входящих в обучающей и тестирующей выборках
-     *
-     * @return параметр средства кросс-валидации
+     * Значение по умолчанию нижней границы интервального значения параметра, задающего количество генерируемых выборок
      */
-    MutableCrossValidatorParameter<Integer> newTrainingTestingSetsSizeRatioParameter();
+    public static final int SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_LOWER_BOUND = 1;
 
     /**
-     * Создание параметра, задающего соотношение количества точных и интервальных экспертных оценок
+     * Значение по умолчанию верхней границы интервального значения параметра, задающего количество генерируемых выборок
+     */
+    public static final int SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_UPPER_BOUND = Integer.MAX_VALUE;
+
+    /**
+     * Значение по умолчанию величины изменения параметра, задающего количество генерируемых выборок,
+     * в процессе кросс-валидации
+     */
+    public static final int SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_STEP_SIZE = 1;
+
+    /**
+     * Минимально допустимое значение параметра, задающего количество генерируемых выборок
+     */
+    public static final int SAMPLES_TO_GENERATE_COUNT_PARAM_MIN = 1;
+
+    /**
+     * Максимально допустимое значение параметра, задающего количество генерируемых выборок
+     */
+    public static final int SAMPLES_TO_GENERATE_COUNT_PARAM_MAX = Integer.MAX_VALUE;
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Идентификатор параметра, задающего количество объектов в генерируемой выборке
+     */
+    public static final String SAMPLE_SIZE_PARAM_ID = "samplesToGenerateCount";
+
+    /**
+     * Значение по умолчанию параметра, задающего количество объектов в генерируемой выборке
+     */
+    public static final int SAMPLE_SIZE_PARAM_DEFAULT_VALUE = 1000;
+
+    /**
+     * Значение по умолчанию нижней границы интервального значения параметра, задающего количество объектов в генерируемой выборке
+     */
+    public static final int SAMPLE_SIZE_PARAM_DEFAULT_LOWER_BOUND = 1;
+
+    /**
+     * Значение по умолчанию верхней границы интервального значения параметра, задающего количество объектов в генерируемой выборке
+     */
+    public static final int SAMPLE_SIZE_PARAM_DEFAULT_UPPER_BOUND = Integer.MAX_VALUE;
+
+    /**
+     * Значение по умолчанию величины изменения параметра, задающего количество объектов в генерируемой выборке,
+     * в процессе кросс-валидации
+     */
+    public static final int SAMPLE_SIZE_PARAM_PARAM_DEFAULT_STEP_SIZE = 1;
+
+    /**
+     * Минимально допустимое значение параметра, задающего количество объектов в генерируемой выборке
+     */
+    public static final int SAMPLE_SIZE_PARAM_MIN = 1;
+
+    /**
+     * Максимально допустимое значение параметра, задающего количество объектов в генерируемой выборке
+     */
+    public static final int SAMPLE_SIZE_PARAM_MAX = Integer.MAX_VALUE;
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Идентификатор параметра, задающего соотношение количества объектов, входящих в обучающей и тестирующей выборках
+     */
+    public static final String TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_ID = "trainingTestingSetsSizeRatio";
+
+    /**
+     * Значение по умолчанию параметра, задающего соотношение количества объектов, входящих в обучающей и тестирующей выборках
+     */
+    public static final int TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_VALUE = 30;
+
+    /**
+     * Значение по умолчанию нижней границы интервального значения параметра, задающего соотношение количества объектов,
+     * входящих в обучающей и тестирующей выборках
+     */
+    public static final int TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_LOWER_BOUND = 5;
+
+    /**
+     * Значение по умолчанию верхней границы интервального значения параметра, задающего соотношение количества объектов,
+     * входящих в обучающей и тестирующей выборках
+     */
+    public static final int TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_UPPER_BOUND = 100;
+
+    /**
+     * Значение по умолчанию величины изменения параметра, задающего соотношение количества объектов,
+     * входящих в обучающей и тестирующей выборках, в процессе кросс-валидации
+     */
+    public static final int TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_STEP_SIZE = 1;
+
+    /**
+     * Минимально допустимое значение параметра, задающего соотношение количества объектов, входящих в обучающей
+     * и тестирующей выборкахе
+     */
+    public static final int TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_MIN = 1;
+
+    /**
+     * Максимально допустимое значение параметра, задающего соотношение количества объектов, входящих в обучающей
+     * и тестирующей выборках
+     */
+    public static final int TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_MAX = 100;
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Идентификатор параметра, задающего соотношение количества точных и интервальных экспертных оценок
+     */
+    public static final String PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_ID = "preciseIntervalJudgmentsCountRatio";
+
+    /**
+     * Значение по умолчанию параметра, задающего соотношение количества точных и интервальных экспертных оценок
+     */
+    public static final int PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_VALUE = 40;
+
+    /**
+     * Значение по умолчанию нижней границы интервального значения параметра, задающего соотношение количества точных
+     * и интервальных экспертных оценок
+     */
+    public static final int PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_LOWER_BOUND = 0;
+
+    /**
+     * Значение по умолчанию верхней границы интервального значения параметра, задающего соотношение количества точных
+     * и интервальных экспертных оценок
+     */
+    public static final int PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_UPPER_BOUND = 100;
+
+    /**
+     * Значение по умолчанию величины изменения параметра, задающего соотношение количества точных
+     * и интервальных экспертных оценок, в процессе кросс-валидации
+     */
+    public static final int PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_STEP_SIZE = 1;
+
+    /**
+     * Минимально допустимое значение параметра, задающего соотношение количества точных и интервальных экспертных оценок
+     */
+    public static final int PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_MIN = 0;
+
+    /**
+     * Максимально допустимое значение параметра, задающего соотношение количества точных и интервальных экспертных оценок
+     */
+    public static final int PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_MAX = 100;
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Создание параметра, задающего количество генерируемых выборок, со значениями по умолчанию
      *
      * @return параметр средства кросс-валидации
      */
-    MutableCrossValidatorParameter<Integer> newPreciseIntervalJudgmentsCountRatioParameter();
+    CrossValidatorParameter<Integer> newSamplesToGenerateCountParameter();
+
+    /**
+     * Создание параметра, задающего количество генерируемых выборок, с заданным значением
+     *
+     * @param value      значение параметра
+     * @param lowerBound значение нижней границы интервального значения параметра
+     * @param upperBound значение верхней границы интервального значения параметра
+     * @param stepSize   величина изменения значения параметра в процессе кросс-валидации
+     * @return параметр средства кросс-валидации
+     * @throws IllegalArgumentException если заданное значение параметра меньше минимально допустимого
+     *                                  или больше максимально допустимого
+     */
+    CrossValidatorParameter<Integer> newSamplesToGenerateCountParameter(int value, int lowerBound,
+                                                                        int upperBound, int stepSize);
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Создание параметра, задающего количество объектов в генерируемой выборке, со значениями по умолчанию
+     *
+     * @return параметр средства кросс-валидации
+     */
+    CrossValidatorParameter<Integer> newSampleSizeParameter();
+
+    /**
+     * Создание параметра, задающего количество объектов в генерируемой выборке, с заданным значением
+     *
+     * @param value      значение параметра
+     * @param lowerBound значение нижней границы интервального значения параметра
+     * @param upperBound значение верхней границы интервального значения параметра
+     * @param stepSize   величина изменения значения параметра в процессе кросс-валидации
+     * @return параметр средства кросс-валидации
+     * @throws IllegalArgumentException если заданное значение параметра меньше минимально допустимого
+     *                                  или больше максимально допустимого
+     */
+    CrossValidatorParameter<Integer> newSampleSizeParameter(int value, int lowerBound,
+                                                            int upperBound, int stepSize);
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Создание параметра, задающего соотношение количества объектов, входящих в обучающей и тестирующей выборках,
+     * со значениями по умолчанию
+     *
+     * @return параметр средства кросс-валидации
+     */
+    CrossValidatorParameter<Integer> newTrainingTestingSetsSizeRatioParameter();
+
+    /**
+     * Создание параметра, задающего соотношение количества объектов, входящих в обучающей и тестирующей выборках,
+     * с заданным значением
+     *
+     * @param value      значение параметра
+     * @param lowerBound значение нижней границы интервального значения параметра
+     * @param upperBound значение верхней границы интервального значения параметра
+     * @param stepSize   величина изменения значения параметра в процессе кросс-валидации
+     * @return параметр средства кросс-валидации
+     * @throws IllegalArgumentException если заданное значение параметра меньше минимально допустимого
+     *                                  или больше максимально допустимого
+     */
+    CrossValidatorParameter<Integer> newTrainingTestingSetsSizeRatioParameter(int value, int lowerBound,
+                                                                              int upperBound, int stepSize);
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * Создание параметра, задающего соотношение количества точных и интервальных экспертных оценок,
+     * со значениями по умолчанию
+     *
+     * @return параметр средства кросс-валидации
+     */
+    CrossValidatorParameter<Integer> newPreciseIntervalJudgmentsCountRatioParameter();
+
+    /**
+     * Создание параметра, задающего соотношение количества точных и интервальных экспертных оценок, с заданным значением
+     *
+     * @param value      значение параметра
+     * @param lowerBound значение нижней границы интервального значения параметра
+     * @param upperBound значение верхней границы интервального значения параметра
+     * @param stepSize   величина изменения значения параметра в процессе кросс-валидации
+     * @return параметр средства кросс-валидации
+     * @throws IllegalArgumentException если заданное значение параметра меньше минимально допустимого
+     *                                  или больше максимально допустимого
+     */
+    CrossValidatorParameter<Integer> newPreciseIntervalJudgmentsCountRatioParameter(int value, int lowerBound,
+                                                                                    int upperBound, int stepSize);
 }
--- svm-analytics/src/main/java/ru/spbftu/igorbotian/phdapp/svm/analytics/CrossValidatorParameterFactoryImpl.java
+++ svm-analytics/src/main/java/ru/spbftu/igorbotian/phdapp/svm/analytics/CrossValidatorParameterFactoryImpl.java
@@ -5,27 +5,76 @@ package ru.spbftu.igorbotian.phdapp.svm.analytics;
  */
 class CrossValidatorParameterFactoryImpl implements CrossValidatorParameterFactory {
 
+
     @Override
-    public MutableCrossValidatorParameter<Integer> newSamplesToGenerateCountParameter() {
-        return new DefaultCrossValidatorParameterImpl<>("samplesToGenerateCount", Integer.class, 100,
-                1, (int) Short.MAX_VALUE, 1, (int) Short.MAX_VALUE, 1, Integer::compare);
+    public CrossValidatorParameter<Integer> newSamplesToGenerateCountParameter() {
+        return newSamplesToGenerateCountParameter(
+                SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_VALUE,
+                SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_LOWER_BOUND,
+                SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_UPPER_BOUND,
+                SAMPLES_TO_GENERATE_COUNT_PARAM_DEFAULT_STEP_SIZE
+        );
     }
 
     @Override
-    public MutableCrossValidatorParameter<Integer> newSampleSizeParameter() {
-        return new DefaultCrossValidatorParameterImpl<>("sampleSize", Integer.class, 1000,
-                1, (int) Short.MAX_VALUE, 1, (int) Short.MAX_VALUE, 1, Integer::compare);
+    public CrossValidatorParameter<Integer> newSamplesToGenerateCountParameter(int value, int lowerBound,
+                                                                               int upperBound, int stepSize) {
+        return new DefaultCrossValidatorParameterImpl<>(SAMPLES_TO_GENERATE_COUNT_PARAM_ID, Integer.class,
+                value, lowerBound, upperBound, SAMPLES_TO_GENERATE_COUNT_PARAM_MIN, SAMPLES_TO_GENERATE_COUNT_PARAM_MAX,
+                stepSize, Integer::compare);
     }
 
     @Override
-    public MutableCrossValidatorParameter<Integer> newTrainingTestingSetsSizeRatioParameter() {
-        return new DefaultCrossValidatorParameterImpl<>("trainingTestingSetsSizeRatio", Integer.class, 30,
-                5, 100, 1, 100, 1, Integer::compare);
+    public CrossValidatorParameter<Integer> newSampleSizeParameter() {
+        return newSamplesToGenerateCountParameter(
+                SAMPLE_SIZE_PARAM_DEFAULT_VALUE,
+                SAMPLE_SIZE_PARAM_DEFAULT_LOWER_BOUND,
+                SAMPLE_SIZE_PARAM_DEFAULT_UPPER_BOUND,
+                SAMPLE_SIZE_PARAM_PARAM_DEFAULT_STEP_SIZE
+        );
     }
 
     @Override
-    public MutableCrossValidatorParameter<Integer> newPreciseIntervalJudgmentsCountRatioParameter() {
-        return new DefaultCrossValidatorParameterImpl<>("preciseIntervalJudgmentsCountRatio", Integer.class, 40,
-                0, 100, 0, 100, 1, Integer::compare);
+    public CrossValidatorParameter<Integer> newSampleSizeParameter(int value, int lowerBound, int upperBound,
+                                                                   int stepSize) {
+        return new DefaultCrossValidatorParameterImpl<>(SAMPLE_SIZE_PARAM_ID, Integer.class,
+                value, lowerBound, upperBound, SAMPLE_SIZE_PARAM_MIN, SAMPLE_SIZE_PARAM_MAX,
+                stepSize, Integer::compare);
+    }
+
+    @Override
+    public CrossValidatorParameter<Integer> newTrainingTestingSetsSizeRatioParameter() {
+        return newSamplesToGenerateCountParameter(
+                TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_VALUE,
+                TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_LOWER_BOUND,
+                TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_UPPER_BOUND,
+                TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_DEFAULT_STEP_SIZE
+        );
+    }
+
+    @Override
+    public CrossValidatorParameter<Integer> newTrainingTestingSetsSizeRatioParameter(int value, int lowerBound,
+                                                                                     int upperBound, int stepSize) {
+        return new DefaultCrossValidatorParameterImpl<>(TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_ID, Integer.class,
+                value, lowerBound, upperBound, TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_MIN,
+                TRAINING_TESTING_SETS_SIZE_RATIO_PARAM_MAX, stepSize, Integer::compare);
+    }
+
+    @Override
+    public CrossValidatorParameter<Integer> newPreciseIntervalJudgmentsCountRatioParameter() {
+        return newSamplesToGenerateCountParameter(
+                PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_VALUE,
+                PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_LOWER_BOUND,
+                PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_UPPER_BOUND,
+                PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_DEFAULT_STEP_SIZE
+        );
+    }
+
+    @Override
+    public CrossValidatorParameter<Integer> newPreciseIntervalJudgmentsCountRatioParameter(int value, int lowerBound,
+                                                                                           int upperBound, int stepSize) {
+        return new DefaultCrossValidatorParameterImpl<>(PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_ID, Integer.class,
+                value, lowerBound, upperBound, PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_MIN,
+                PRECISE_INTERVAL_JUDGEMENTS_COUNT_RATIO_PARAM_MAX, stepSize, Integer::compare);
     }
 }
